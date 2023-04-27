0c2bc4921fdded34c082a67fef9f709eda42fbd0
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-27 22:59:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const URL1 = 'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages';
+const URL2 = 'https://restcountries.com/v3.1/name/{name}';
 
-// https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages
-
+function fetchCountries(){
+fetch(URL1).then(resp => {if
+    (!resp.ok){
+throw new error(resp.statusText)
+    }
+    return resp.json()
+}).then()
+.catch(error)
+};
+fetchCountries()
 // document.addEventListener(
 //     "input",
 //     _.debounce(() => {
