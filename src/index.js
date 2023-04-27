e6107209db616b28b8caedca122b8ba5b58514a8
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-28 00:16:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const URL1 = 'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages';
-const URL2 = 'https://restcountries.com/v3.1/name/{name}';
+const BASE_URL = 'https://restcountries.com/v3.1';
+const ENDPOINT = '/all?fields=name,capital,population,flags,languages';
+const input = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+document.addEventListener('input', debounce(onSearch,DEBOUNCE_DELAY));
+
+function onSearch(evt){
+    evt.preventDefault();
+console.dir(evt.target);
+    // const name = evt.target.value.trim();
+}
 
 function fetchCountries(){
-fetch(URL1).then(resp => {if
+    const URL = `${BASE_URL}${ENDPOINT}`;
+    return fetch(URL).then(resp => {if
     (!resp.ok){
 throw new error(resp.statusText)
     }
     return resp.json()
-}).then()
-.catch(error)
+})
 };
-fetchCountries()
-// document.addEventListener(
-//     "input",
-//     _.debounce(() => {
-     
-//     }, 300)
-//   );
+
+fetchCountries().then(data => console.log(data)).catch(error =>console.log(error))
+
+
