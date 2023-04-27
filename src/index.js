ac5b40cc97f6928a20d44076d9a236dc0d5e608f
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-27 18:30:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+// https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages
+
+// document.addEventListener(
+//     "input",
+//     _.debounce(() => {
+     
+//     }, 300)
+//   );
