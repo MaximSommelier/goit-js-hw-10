1d5833fe5e8f648bab2ddb003e19ca09e8411dec
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-29 16:45:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,38 +3,46 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-const BASE_URL = 'https://restcountries.com/v3.1';
-const ENDPOINT = '/all?fields=name,capital,population,flags,languages';
-const input = document.querySelector("#search-box");
-const countryList = document.querySelector(".country-list");
-const countryInfo = document.querySelector(".country-info");
+// const BASE_URL = 'https://restcountries.com/v3.1';
+// const ENDPOINT = '/all?fields=name,capital,population,flags,languages';
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-document.addEventListener('input', debounce(onSearch,DEBOUNCE_DELAY));
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 const nameSearch = evt.target.value.trim();
 const littleList = document.querySelector('.country-list');
 
-function onSearch(nameSearch){
-    evt.preventDefault();
-    const {name,capital,population,flags,languages} = evt.currentTarget.elements;
-    fetchCountries().then(data => console.log(data)).catch(error =>console.log(error))
-    console.dir(name);
+function onSearch(evt) {
+  evt.preventDefault();
+  const { name, capital, population, flags, languages } =
+    evt.currentTarget.elements;
+  fetchCountries()
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+  console.dir(name);
 }
 
-function fetchCountries(){
-    const URL = `${BASE_URL}${ENDPOINT}`;
-    return fetch(URL).then(resp => {if
-    (!resp.ok){
-throw new error(resp.statusText)
+function fetchCountries(name) {
+  const URL = `https://restcountries.com/v3.1/${name}?fields=name,capital,population,flags,languages`;
+  return fetch(URL).then(resp => {
+    if (!resp.ok) {
+      throw new error(resp.statusText);
     }
-    return resp.json()
-})
-};
+    return resp.json();
+  });
+}
 
-function createLittleMarkup (arr){
-    arr.map(({flags:{svg}, name:{official}}) => `<li><img src="${svg}" alt="flag">
+function createLittleMarkup(arr) {
+  return arr
+    .map(
+      ({
+        flags: { svg },
+        name: { official },
+      }) => `<li><img src="${svg}" alt="${official}">
     <p>${official}</p>
-  </li>`).join('')
-    // const littleMarkup = 
-};
-
-
+  </li>`
+    )
+    .join('');
+    
+}
