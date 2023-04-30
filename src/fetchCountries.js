d2251dd710d6d6d66f96772a133f20794099080d
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-30 23:41:41 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,7 +4,7 @@ function fetchCountries(name) {
     const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
     return fetch(URL).then(resp => {
       if (!resp.ok) {
-        throw new error(Notiflix.Notify.failure(`Oops, there is no country with that name.`));
+        throw new Error(Notiflix.Notify.failure(`Oops, there is no country with that name.`));
       }
       return resp.json();
     });
